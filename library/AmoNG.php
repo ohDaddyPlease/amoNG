b4158cb8e643cfdc590b82060018ffb079707fc5
Serj <kraspvp@mail.ru>
2019-12-17 12:19:32 +0700

    Переформатировал и заккоментил класс AmoNG

--- library/AmoNG.php
+++ library/AmoNG.php
@@ -4,17 +4,55 @@ namespace AmoNG;
 use AmoNG\Account;
 use AmoNG\Logger;
 
+/**
+ * главный класс, являющийся входной точкой,
+ * объединяет все инициализации классов
+ */
 class AmoNG
 {
+  /**
+   * экземпляр класса Account
+   */
   public $account;
+
+  /**
+   * экземпляр класса Request
+   */
   public $request;
+  
+  /**
+   * экземпляр класса Logger
+   */
   public $logger;
+
+  /**
+   * экземпляр класса Authorization
+   */
   public $auth;
+
+  /**
+   * экземпляр класса Leads
+   */
   public $leads;
+
+  /**
+   * экземпляр класса Contacts
+   */
   public $contacts;
+
+  /**
+   * подгружаемые модули, передается строкой
+   */
   private $modules;
 
   public function __construct(string $modules){
+
+    /**
+     * подгружаем модули:
+     * - разбиваем строку на модули
+     * - обтрезаем пробелы и занижаем
+     * - меняем местами ключ-значение на значение-ключ
+     */
     $this->modules = explode(',', $modules);
     foreach($this->modules as $key => $module)
     {
@@ -22,13 +60,12 @@ class AmoNG
     }
     $this->modules = array_flip($this->modules);
 
+    // в зависимости от написанных модулей подключаем их
     if(key_exists('full', $this->modules))
     {
       $this->account = new Account();
-
       $this->auth = new Authorization;
       $this->request = new Request($this->auth);
-
       $this->logger = new Logger;
       $this->leads = new Leads;
       $this->contacts = new Contacts;
@@ -36,8 +73,5 @@ class AmoNG
 
       return;
     }
-
-
   }
-
 }
